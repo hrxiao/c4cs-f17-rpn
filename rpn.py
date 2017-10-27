3e269acf323e58ff7eb48ba8b66527eeb15f07d1
Haoran Xiao <hxiao7463@gmail.com>
2017-10-27 12:36:14 -0400

    end of class commit

--- rpn.py
+++ rpn.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python3
+import operator
+
+ops = {
+	'+': operator.add,
+	'-': operator.sub
+}
+
+#dict(operator)???
 
 def calculate(arg):
 	stack = list()
 	for token in arg.split():
-		if token == '+':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg1 + arg2
-			stack.append(result)
-		elif token == "-":
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg2 - arg1
-			stack.append(result)
-		else:
+		try:
 			stack.append(int(token))
+		except ValueError:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			function = ops(token)
+			result = function(arg1, arg2)
+			stack.append(result)
+			
+		# if token == '+':
+		# 	arg1 = stack.pop()
+		# 	arg2 = stack.pop()
+		# 	result = arg1 + arg2
+		# 	stack.append(result)
+		# elif token == "-":
+		# 	arg1 = stack.pop()
+		# 	arg2 = stack.pop()
+		# 	result = arg2 - arg1
+		# 	stack.append(result)
+		# else:
 	# print(stack)
 	return stack.pop()
 
