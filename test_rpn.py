4a2fda0456af8a243f95ec9be8fc1c8fa2eb6d3f
Haoran Xiao <hxiao7463@gmail.com>
2017-10-27 12:05:44 -0400

    added test harness

--- test_rpn.py
+++ test_rpn.py
@@ -1,7 +1,9 @@
 import unittest # turns it into obj named unittest in this code
 import rpn
 
+# python3 -m unittest
+
 class TestBasics(unittest.TestCase):
 	def test_add(self):
-		result = rpn.calculate("1 1 +")
+		result = rpn.calculate('1 1 +')
 		self.assertEqual(2,result)
