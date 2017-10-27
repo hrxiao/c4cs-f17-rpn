9d4addcbbcfffbd5570e30265eef2f6c1e037753
Haoran Xiao <hxiao7463@gmail.com>
2017-10-27 11:50:28 -0400

    initial commit

---
+++ rpn.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def calculate(arg):
+	pass
+
+def main():
+	while True:
+		calculate(input("rpn calc> "))
+
+if __name__ == '__main__': # __name__ stores the main thing being run
+	# will not run if running from test_rpn.py
+	main()
---
+++ test_rpn.py
@@ -0,0 +1,7 @@
+import unittest # turns it into obj named unittest in this code
+import rpn
+
+class TestBasics(unittest.TestCase):
+	def test_add(self):
+		result = rpn.calculate("1 1 +")
+		self.assertEqual(2,result)
