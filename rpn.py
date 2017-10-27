f1d5870a6bd469f4fe2316d12e34c85b269cadd2
Haoran Xiao <hxiao7463@gmail.com>
2017-10-27 18:14:35 -0400

    added color support

--- rpn.py
+++ rpn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import operator
 import readline
+from colorama import init, Fore, Back, Style
 
 ops = {
 	'+': operator.add,
@@ -15,6 +16,16 @@ def calculate(arg):
 	stack = list()
 	for token in arg.split():
 		try:
+			num_token = int(token)
+			if num_token < 0:
+				print(Fore.WHITE + Back.RED + token + Style.RESET_ALL, end=' ')
+			else:
+				print(Fore.RED + Back.WHITE + token + Style.RESET_ALL, end=' ')
+		except ValueError:
+			print(Fore.WHITE + Back.BLUE + token + Style.RESET_ALL, end=' ')
+	print('=', end=' ')
+	for token in arg.split():
+		try:
 			stack.append(int(token))
 		except ValueError:
 			arg2 = stack.pop()
@@ -22,18 +33,18 @@ def calculate(arg):
 			function = ops[token]
 			result = function(arg1, arg2)
 			stack.append(result)
-	print(stack)
+	print(str(stack[0]))
 	return stack.pop()
 
 def main():
 	while True:
 		calculate(input("rpn calc> "))
 
-def function_without_a_test():
-	a = 1
-	b = 2
-	c = 3
-	d = a + b + c
+# def function_without_a_test():
+# 	a = 1
+# 	b = 2
+# 	c = 3
+# 	d = a + b + c
 
 if __name__ == '__main__': # __name__ stores the main thing being run
 	# will not run if running from test_rpn.py
